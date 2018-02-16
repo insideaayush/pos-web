6fadfb5479b2ab3a3bb9cfc6950a25722ee442b6
Aayush Gautam <aayush.gautam924@gmail.com>
2018-02-16 19:04:43 +0530

    Set up React for production

--- pos_api/settings/production.py
+++ pos_api/settings/production.py
@@ -20,6 +20,18 @@ DATABASES = {
     }
 }
 
+# React Settings
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
+        }
+}
+
 ###################################
 # s3 storage
 ###################################
@@ -49,3 +61,5 @@ MEDIA_DIRECTORY = '/media/'
 
 STATIC_URL = S3_URL + STATIC_DIRECTORY
 MEDIA_URL = S3_URL + MEDIA_DIRECTORY
+
+
